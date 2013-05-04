8280ff6a4e313209a8cd2195f7fb512c36172d66
EDH <eric.delahoupliere@free.fr>
2013-05-04 23:17:49 +0200

    adapt MPU libs to I2C Chipkit

--- libraries/MPU6050/MPU6050.cpp
+++ libraries/MPU6050/MPU6050.cpp
@@ -35,8 +35,14 @@ THE SOFTWARE.
 */
 
 #include "MPU6050.h"
+#include <Wire.h>     // used for I2C protocol (lib)
 
 
+/** Default timeout value for read operations.
+* Set this to 0 to disable timeout detection.
+*/
+uint16_t I2Cdev::readTimeout = I2CDEV_DEFAULT_READ_TIMEOUT;
+    
 /** Read a single bit from an 8-bit device register.
  * @param devAddr I2C slave device address
  * @param regAddr Register regAddr to read from
@@ -149,7 +155,59 @@ int8_t I2Cdev::readWord(uint8_t devAddr, uint8_t regAddr, uint16_t *data, uint16
  * @return Number of bytes read (-1 indicates failure)
  */
 int8_t I2Cdev::readBytes(uint8_t devAddr, uint8_t regAddr, uint8_t length, uint8_t *data, uint16_t timeout) {
+    int8_t count = 0;
+    uint32_t t1 = millis();
+    
+    for (uint8_t k = 0; k < length; k += min(length, BUFFER_LENGTH)) {
+                Wire.beginTransmission(devAddr);
+                Wire.send(regAddr);
+                Wire.endTransmission();
+                delay(1);
+                Wire.requestFrom(devAddr, (uint8_t)min(length - k, BUFFER_LENGTH));
+
+                for (; Wire.available() && (timeout == 0 || millis() - t1 < timeout); count++) {
+                    data[count] = Wire.receive();
+                }
+    } 
+    
+    // check for timeout
+    if (timeout > 0 && millis() - t1 >= timeout && count < length) count = -1; // timeout
+
+    return count;           
 } 
+
+int8_t I2Cdev::readWords(uint8_t devAddr, uint8_t regAddr, uint8_t length, uint16_t *data, uint16_t timeout) {
+    int8_t count = 0;
+    uint32_t t1 = millis();
+    
+for (uint8_t k = 0; k < length * 2; k += min(length * 2, BUFFER_LENGTH)) {
+                Wire.beginTransmission(devAddr);
+                Wire.send(regAddr);
+                Wire.endTransmission();
+                delay(1);
+                Wire.requestFrom(devAddr, (uint8_t)(length * 2)); // length=words, this wants bytes
+
+                bool msb = true; // starts with MSB, then LSB
+                for (; Wire.available() && count < length && (timeout == 0 || millis() - t1 < timeout);) {
+                    if (msb) {
+                        // first byte is bits 15-8 (MSb=15)
+                        data[count] = Wire.receive() << 8;
+                    } else {
+                        // second byte is bits 7-0 (LSb=0)
+                        data[count] |= Wire.receive();
+                        count++;
+                    }
+                    msb = !msb;
+                }
+    } 
+    
+    // check for timeout
+    if (timeout > 0 && millis() - t1 >= timeout && count < length) count = -1; // timeout
+
+    return count;           
+} 
+
+
 
 /** write a single bit in an 8-bit device register.
  * @param devAddr I2C slave device address
@@ -265,7 +323,32 @@ bool I2Cdev::writeWord(uint8_t devAddr, uint8_t regAddr, uint16_t data) {
  * @return Status of operation (true = success)
  */
 bool I2Cdev::writeBytes(uint8_t devAddr, uint8_t regAddr, uint8_t length, uint8_t* data) {
+    uint8_t status = 0;
+    Wire.beginTransmission(devAddr);
+    Wire.send((uint8_t) regAddr); // send address
+ 
+    for (uint8_t i = 0; i < length; i++) {
+            Wire.send((uint8_t) data[i]);
+    }
+ 
+    Wire.endTransmission();
+    return status == 0;       
 }  
+
+bool I2Cdev::writeWords(uint8_t devAddr, uint8_t regAddr, uint8_t length, uint16_t* data) {
+    uint8_t status = 0;
+ 
+    Wire.beginTransmission(devAddr);
+    Wire.send(regAddr); // send address
+ 
+     for (uint8_t i = 0; i < length * 2; i++) {
+            Wire.send((uint8_t)(data[i++] >> 8)); // send MSB
+            Wire.send((uint8_t)data[i]); // send LSB
+    }
+    
+    Wire.endTransmission();
+    return status == 0;
+}
 
 /** Default constructor, uses default I2C address.
  * @see MPU6050_DEFAULT_ADDRESS
